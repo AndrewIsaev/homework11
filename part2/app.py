b288ca7dd88e3fbecf9ed83de4aacc5d98f03f76
andrey.isaev <an.isaev7@gmail.com>
2022-09-10 00:24:12 +0300

    fix: refactor code

--- part2/app.py
+++ part2/app.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template
 from part2 import utils
 
-PATH = "candidates.json"
 app = Flask(__name__)
 
 
 @app.route("/")
 def all_candidates():
-    candidates = utils.load_candidates_from_json(PATH)
+    candidates = utils.load_candidates_from_json()
     return render_template("list.html", candidates=candidates)
 
 
 @app.route("/candidates/<int:uid>")
 def candidates_by_pk(uid):
     candidate = utils.get_candidate(uid)
-    return render_template("single.html", candidate=candidate)
+    is_exist = candidate != None
+    return render_template("single.html", candidate=candidate,
+                           is_exist=is_exist)
 
 
 @app.route("/search/<candidate_name>")
@@ -26,7 +27,9 @@ def search(candidate_name):
 @app.route("/skill/<skill_name>")
 def search_by_skill(skill_name):
     candidates = utils.get_candidate_by_skill(skill_name)
-    return render_template("skill.html", candidates=candidates, skill=skill_name, quantity = len(candidates))
+    return render_template("skill.html", candidates=candidates,
+                           skill=skill_name, quantity=len(candidates))
+
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
