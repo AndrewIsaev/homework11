79846c98726351e45003d273f8fce4cb59d921a5
andrey.isaev <an.isaev7@gmail.com>
2022-09-09 11:29:56 +0300

    feat: add get_candidate

--- part2/utils.py
+++ part2/utils.py
@@ -7,8 +7,14 @@ def load_candidates_from_json(path:str):
     """
     Return list of all candidates from json file
     """
-
     with open(path, encoding="utf-8") as file:
         return json.load(file)
+
+
+def get_candidate(candidate_id:int):
+    for candidate in load_candidates_from_json("candidates.json"):
+        if candidate["id"] == candidate_id:
+            return candidate
+        return "Нет такого кандидата"
 
 
