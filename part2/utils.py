ef28b32735803cb68d49815210c8acd53a6030e0
andrey.isaev <an.isaev7@gmail.com>
2022-09-09 11:40:18 +0300

    fix: add get_candidate, add get_candidates_by_name

--- part2/utils.py
+++ part2/utils.py
@@ -1,9 +1,9 @@
 import json
+from typing import List, Dict
 
+PATH = "candidates.json"
 
-
-
-def load_candidates_from_json(path:str):
+def load_candidates_from_json(path:str)-> List[dict]:
     """
     Return list of all candidates from json file
     """
@@ -11,10 +11,18 @@ def load_candidates_from_json(path:str):
         return json.load(file)
 
 
-def get_candidate(candidate_id:int):
-    for candidate in load_candidates_from_json("candidates.json"):
+def get_candidate(candidate_id:int)->Dict:
+    for candidate in load_candidates_from_json(PATH):
         if candidate["id"] == candidate_id:
             return candidate
-        return "Нет такого кандидата"
+    return "Нет такого кандидата"
 
 
+def get_candidates_by_name(candidate_name: str) -> Dict:
+    for candidate in load_candidates_from_json(PATH):
+        if candidate["name"] == candidate_name:
+            return candidate
+    return "Нет такого кандидата"
+
+
+print(get_candidates_by_name("Burt Stein"))
